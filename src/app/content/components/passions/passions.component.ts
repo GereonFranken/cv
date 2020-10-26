121038933d59a05af5a81a92a8838b7cfcfca107
GereonFranken <GereonFranken@users.noreply.github.com>
2020-10-26 21:44:13 +0100

    create first passion layout

--- src/app/content/components/passions/passions.component.ts
+++ src/app/content/components/passions/passions.component.ts
@@ -1,26 +1,68 @@
 import { Component, OnInit } from '@angular/core';
-import { FaIconLibrary, FontAwesomeModule } from "@fortawesome/angular-fontawesome";
-import { faChess } from '@fortawesome/free-solid-svg-icons';
+import { FaIconLibrary } from "@fortawesome/angular-fontawesome";
+import { faChess, faFutbol, faLaptop, faHeartbeat, faMusic } from '@fortawesome/free-solid-svg-icons';
 
+export enum Passion {
+  none = '',
+  chess = 'Chess', 
+  gaming = 'Gaming',
+  soccer = 'Soccer',
+  fitness = 'Fitness',
+  music = 'Music',
+}
 @Component({
-  selector: 'app-hobbies',
+  selector: 'app-passions',
   templateUrl: './passions.component.html',
   styleUrls: ['./passions.component.scss']
 })
+
 export class PassionsComponent implements OnInit {
 
+  passion: Passion = Passion.none;
+  // len(Passion) -1 is num of all hobbies/ number of sides of the created polygon
+  numSides = Object.keys(Passion).length -1;
+  // (numSides -2) * 180 is the formula for the sum of angles in a polygon
+  anglesSum = (this.numSides - 2) * 180;
+  angle = 180 - this.anglesSum / this.numSides;
+  polygonDistance = 120 //px
+  icons = [
+    {
+      'iconName': 'chess',
+      'passion': 'chess',
+    },
+    {
+      'iconName': 'futbol',
+      'passion': 'soccer',
+    },
+    {
+      'iconName': 'laptop',
+      'passion': 'gaming',
+    },
+    {
+      'iconName': 'heartbeat',
+      'passion': 'fitness',
+    },
+    {
+      'iconName': 'music',
+      'passion': 'music',
+    },
+  ]
+
   constructor(private library: FaIconLibrary) {
-    this.library.addIcons(faChess);
+    this.library.addIcons(faChess, faFutbol, faLaptop, faHeartbeat, faMusic);
   }
 
-  ngOnInit(): void { }
+  calculateVector(angle: number) {
+    // vx of vector v is calculated by |v| * cos(angle) + half of the icon width to center it
+    // vy of vector v is calculated by |v| * sin(angle) + half of the icon height to center it
+    return {x: (this.polygonDistance * Math.sin(angle*Math.PI/180) - 21),
+            y: (-(this.polygonDistance * Math.cos(angle*Math.PI/180) + 21))};
+  }
 
-  /**
-   * Fußball
-   * Fitness
-   * Zocken
-   * Schach
-   * Piano/ Music
-   */
+  ngOnInit(): void {;
+  }
 
+  handleIconClick(clickedPassion: Passion) {
+    this.passion = clickedPassion;
+  }
 }
