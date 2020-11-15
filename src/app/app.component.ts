e224ec0615e6a34f87df5cff44130b80e588310e
GereonFranken <GereonFranken@users.noreply.github.com>
2020-11-15 09:16:41 +0100

    include new background style

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,18 @@
+import { animate, style, transition, trigger } from '@angular/animations';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  animations: [
+    trigger('backgroundMovement', [
+      transition(':enter', [
+        style({ position: 'absolute' }),
+        animate('20s', style({ transform: 'translateY(50%) scale(2)', 'z-index': -11})),
+      ]),
+    ]),
+  ],
 })
 export class AppComponent {
   title = 'cv';
