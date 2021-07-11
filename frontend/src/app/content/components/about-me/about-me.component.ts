b3df68e83a3da19fe04fb9c90b7607db27a14947
GereonFranken <GereonFranken@users.noreply.github.com>
2021-07-11 13:10:36 +0200

    Revert "Revert "apply new folder structure to files p1""
    This reverts commit 913a8acbd400e3606573bfa8b99f593d71fca3dd.

---
+++ frontend/src/app/content/components/about-me/about-me.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import { MatRadioChange } from '@angular/material/radio';
+
+@Component({
+  selector: 'app-about-me',
+  templateUrl: './about-me.component.html',
+  styleUrls: ['./about-me.component.scss']
+})
+export class AboutMeComponent implements OnInit {
+
+  selectedLanguage: 'english' | 'german' = 'english';
+  profilbildPath = 'assets\\img\\profilbild1.jpg';
+  defaultChecked = true;
+
+  constructor() { }
+
+  ngOnInit(): void { }
+
+}
