11e74a2542bb57c1c0ef0b7ea3970854631b4ff2
Gereon Franken <gereon.franken@gmx.de>
2021-10-30 15:40:33 +0200

    update styles

--- src/app/content/components/passions/passions.component.ts
+++ src/app/content/components/passions/passions.component.ts
@@ -104,8 +104,8 @@ export class PassionsComponent implements OnInit {
 
   @HostListener('window:resize', ['$event'])
   getScreenWidth(event?) {
-    if (this.onMobile) this.polygonDistance = (0.57 * window.innerWidth) / 2;
-    else this.polygonDistance = (0.3 * window.innerWidth) / 2;
+    if (this.onMobile) this.polygonDistance = (0.61 * window.innerWidth) / 2;
+    else this.polygonDistance = (0.27 * window.innerWidth) / 2;
   }
 
   calculateVector(angle: number) {
